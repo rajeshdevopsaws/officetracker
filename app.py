db7eb01f8eca936ff663266f0046e4f63d1e236f
Kanumuru Rajesh <rajesh@we45.com>
2025-03-25 23:02:36 +0530

    added day option and made 5000 port to run on

--- app.py
+++ app.py
@@ -253,7 +253,7 @@ def export_month(year, month):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Office Tracker Application')
     parser.add_argument('--host', default='0.0.0.0', help='Host to bind to')
-    parser.add_argument('--port', type=int, default=5001, help='Port to bind to')
+    parser.add_argument('--port', type=int, default=5000, help='Port to bind to')
     args = parser.parse_args()
 
     init_db()
